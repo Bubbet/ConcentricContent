e9444b33f79f6cd31e37fba4cf2c44b07c1d39a2
Bubbet <bubbet@bubbet.dev>
2024-09-18 10:48:25 -0600

    Initial Commit

---
+++ CodedAssetsPlugin.cs
@@ -0,0 +1,24 @@
+﻿global using static CodedAssets.CodedAssetsPlugin;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace CodedAssets
+{
+	[BepInPlugin(GUID, "CodedAssets", "1.0.0")]
+	public class CodedAssetsPlugin : BaseUnityPlugin
+	{
+		public static Harmony harm;
+		public static CodedAssetsPlugin instance;
+		public static ManualLogSource log;
+		public const string GUID = "bubbet.codedassets";
+
+		public void Awake()
+		{
+			instance = this;
+			log = Logger;
+			harm = new Harmony(Info.Metadata.GUID);
+			harm.PatchAll();
+		}
+	}
+}
---
+++ ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace CodedAssets
+{
+	public static class ExtensionMethods
+	{
+		public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
+		{
+			var result = gameObject.GetComponent<T>();
+			if (!result) result = gameObject.AddComponent<T>();
+			return result;
+		}
+
+		public static TV GetOrSet<TK, TV>(this Dictionary<TK, TV> dict, TK key, Func<TV> valueGetter)
+		{
+			if (dict.TryGetValue(key, out var value)) return value;
+			value = valueGetter();
+			dict[key] = value;
+			return value;
+		}
+	}
+}
